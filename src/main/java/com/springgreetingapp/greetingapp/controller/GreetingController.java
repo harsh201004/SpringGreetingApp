a8da3be93679efb0cbf4874f9eb4d3c706044693
harsh201004 <harshraghuwanshi2010@gmail.com>
2025-02-24 12:40:24 +0530

    [Harsh]add. Ability for the greeting app to delete a greeting message in the repository

--- src/main/java/com/springgreetingapp/greetingapp/controller/GreetingController.java
+++ src/main/java/com/springgreetingapp/greetingapp/controller/GreetingController.java
@@ -75,4 +75,11 @@ public class GreetingController {
     public Greeting updateGreeting(@PathVariable Long id, @RequestParam String message) {
         return greetingService.updateGreeting(id, message);
     }
+
+    //Work for UC8
+    @DeleteMapping("/delete/{id}")
+    public String deleteGreeting(@PathVariable Long id) {
+        greetingService.deleteGreeting(id);
+        return "Greeting deleted successfully!";
+    }
 }
